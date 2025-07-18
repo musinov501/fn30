462962a04c2d77cc7856630b681b85a046a8c38b
Muhammadyor <musinovmuhammaader@gmail.com>
2025-07-18 15:00:14 +0500

    help handlerini yozdik

--- main.py
+++ main.py
@@ -12,5 +12,16 @@ def start(message: Message):
     bot.send_message(chat_id, "Assalomu alaykum")
 
 
+
+@bot.message_handler(commands=['help'])
+def reaction_to_help(message: Message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Adminga murojat --> @durov")
+
+
+
+
+
+
 if __name__ == '__main__':
     bot.infinity_polling()
